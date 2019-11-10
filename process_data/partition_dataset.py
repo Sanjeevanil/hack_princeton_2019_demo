ba5c8eb04647218ad712298e5847bf69038c0851
Felix Cheng <felixcheng232@gmail.com>
2019-11-09 19:29:06 -0500

    added secondary categories, auto-remove discards

--- process_data/partition_dataset.py
+++ process_data/partition_dataset.py
@@ -8,11 +8,16 @@ dataset = csv.reader(open("./labelled_data.csv", 'r'))
 
 data_by_category = {}
 
+category_idx = 2
+
 for row in dataset:
-	row_category = row[2]
+	row_category = row[category_idx]
 	if row_category not in data_by_category:
 		data_by_category[row_category] = []
-	data_by_category[row_category].append(row[0:2])
+
+	row_contents = row[0:category_idx]
+	row_contents.append(row[category_idx + 1])
+	data_by_category[row_category].append(row_contents)
 
 training_set_file = open("training_set.csv", 'w')
 validation_set_file = open("validation_set.csv", 'w')
@@ -27,6 +32,6 @@ for category_name in data_by_category.keys():
 
 	for item_num in range(num_validation_data_points, total_samples - num_validation_data_points):
 		row = data_by_category[category_name][item_num]
-		training_set_file.write(row[0] + "," + row[1] + "," + category_name + "\n")
+		training_set_file.write(row[0] + "," + row[1] + "," + category_name + "," + row[2] + "\n")
 
 	
