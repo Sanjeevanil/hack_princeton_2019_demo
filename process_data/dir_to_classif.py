b72731be4f489ad653bb80f6621780d59740a7a6
Felix Cheng <felixcheng232@gmail.com>
2019-11-09 19:02:38 -0500

    Added data CSVs

---
+++ process_data/dir_to_classif.py
@@ -0,0 +1,29 @@
+import glob
+import json
+
+data_folder_name = "MPI_label_2"
+json_file_paths = glob.glob("/home/felix-cheng/hpr_test/MPI_label_2/**/*.json", recursive=True)
+category = ""
+path_to_image = ""
+
+
+output_file = open("./labelled_data.csv", 'w')
+
+def replace_spaces_with_hyphens(string):
+	newstr = ""
+	for char in string:
+		if char == " ":
+			newstr += "-"
+		else:	
+			newstr += char
+	return newstr
+
+for file_path in json_file_paths:
+	category_substring_idx_start = file_path.find(data_folder_name) + len(data_folder_name) + 1
+	category_substring_idx_end = file_path.find("/", category_substring_idx_start)
+	category = replace_spaces_with_hyphens(file_path[category_substring_idx_start:category_substring_idx_end])
+	
+	json_contents = json.load(open(file_path))
+	path_to_image = replace_spaces_with_hyphens(json_contents["image_path"])
+
+	output_file.write(replace_spaces_with_hyphens(file_path) + "," + path_to_image + "," + category + "\n")
